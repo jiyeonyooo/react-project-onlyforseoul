db485200447e58ab51e92fb3363f5ae154fae2ce
jiyeonyooo <ji04230423@gmail.com>
2025-07-15 00:08:46 +0900

    feat: add Header component and implement UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
+import './Header.css';
+import logo from '../assets/logo_white.png';
+
 const Header = () => {
     return (
         <>
-            <h1>Hello</h1>
+         <header>
+            <img className="header__logo" src={logo} alt="logo"></img>
+            <nav>
+               <ul>
+                  <li>시즌 통계</li>
+                  <li>경기 일정</li>
+                  <li>선수 정보</li>
+                  <li>응원가</li>
+                  <li>직관 기록</li>
+               </ul>
+            </nav>
+         </header>
         </>
     );
 };
