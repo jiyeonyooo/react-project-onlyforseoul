6c8cdf67571a92c87d6752da470877e6567d42f4
jiyeonyooo <ji04230423@gmail.com>
2025-07-22 23:14:48 +0900

    feat: make click event as routing to homepage on logo image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,25 @@
 import './Header.css';
+import {Link, useNavigate} from 'react-router-dom';
 import logo from '../assets/logo_white.png';
 
 const Header = () => {
+
+   const navigate = useNavigate();
+   const handleLogoClick = () => {
+      navigate("/");
+   };
+
     return (
         <>
          <header>
-            <img className="header__logo" src={logo} alt="logo"></img>
+            <img className="header__logo" onClick={handleLogoClick} src={logo} alt="logo"></img>
             <nav>
                <ul>
-                  <li>시즌 통계</li>
-                  <li>경기 일정</li>
-                  <li>선수 정보</li>
-                  <li>응원가</li>
-                  <li>직관 기록</li>
+                  <li><Link to="/report">시즌 통계</Link></li>
+                  <li><Link to="/schedule">경기 일정</Link></li>
+                  <li><Link to="/players">선수 정보</Link></li>
+                  <li><Link to="/schedule">응원가</Link></li>
+                  <li><Link to="/schedule">직관 기록</Link></li>
                </ul>
             </nav>
          </header>
