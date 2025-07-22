017e50d73d61b7fe8ce4681e1f357da46f93c654
jiyeonyooo <ji04230423@gmail.com>
2025-07-22 15:06:52 +0900

    feat: add Route tag from react-router-dom and set paths for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import './App.css'
-import Header from './components/Header.jsx'
+import './App.css';
+import {Routes, Route, Link} from 'react-router-dom';
+import Header from './components/Header.jsx';
+import HomePage from './pages/HomePage.jsx';
+import ReportPage from './pages/ReportPage.jsx';
+import PlayerInfoPage from './pages/PlayerInfoPage.jsx';
+import MatchSchedulePage from './pages/MatchSchedulePage.jsx';
+import NotFoundPage from './pages/NotFoundPage.jsx';
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/report" element={<ReportPage />} />
+        <Route path="/schedule" element={<MatchSchedulePage />} />
+        <Route path="/players" element={<PlayerInfoPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
   );
 };
