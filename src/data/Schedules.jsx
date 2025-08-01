a8c6990edea111399f89bcc5ef336968682f8990
jiyeonyooo <ji04230423@gmail.com>
2025-08-01 21:28:05 +0900

    feat: make separate file for schedule information

---
+++ src/data/Schedules.jsx
@@ -0,0 +1,20 @@
+const schedules = {
+  "2025-07-16": [
+    { text: "최준영 선수 생일", type: "birth" },
+    { text: "안데르손 선수 생일", type: "birth" }
+  ],
+  "2025-07-20": [
+    { text: "(H) vs 울산 | 22R", type: "match" }
+  ],
+  "2025-07-23": [
+   { text: "(A) vs 제주 | 23R", type: "match" },
+    { text: "최철원 선수 생일", type: "birth" }
+  ],
+  "2025-07-30": [
+    { text: "K리그 vs 뉴캐슬", type: "etc" }
+  ],
+  "2025-07-31": [
+    { text: "서울 vs FC 바르셀로나", type: "match" },
+  ],
+};
+export default schedules;
