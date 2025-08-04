94f92f0e32027f5c54cb5e846bff09c082ed311a
jiyeonyooo <ji04230423@gmail.com>
2025-08-04 23:19:37 +0900

    chore: make kLeague Standings file

---
+++ src/data/LeagueStandings.jsx
@@ -0,0 +1,16 @@
+const LeagueStandings = {
+  "전북": { rank: 1, games: 24, points: 54, win: 16, draw: 6, lose: 2, goalsFor: 43, goalsAgainst: 19, goalDiff: 24 },
+  "김천": { rank: 2, games: 24, points: 39, win: 11, draw: 6, lose: 7, goalsFor: 34, goalsAgainst: 24, goalDiff: 10 },
+  "대전": { rank: 3, games: 24, points: 39, win: 10, draw: 9, lose: 5, goalsFor: 32, goalsAgainst: 28, goalDiff: 4 },
+  "서울": { rank: 4, games: 24, points: 36, win: 9, draw: 9, lose: 6, goalsFor: 27, goalsAgainst: 23, goalDiff: 4 },
+  "포항": { rank: 5, games: 24, points: 35, win: 10, draw: 5, lose: 9, goalsFor: 30, goalsAgainst: 34, goalDiff: -4 },
+  "광주": { rank: 6, games: 24, points: 32, win: 8, draw: 8, lose: 8, goalsFor: 25, goalsAgainst: 28, goalDiff: -3 },
+  "울산": { rank: 7, games: 24, points: 31, win: 8, draw: 7, lose: 9, goalsFor: 30, goalsAgainst: 29, goalDiff: 1 },
+  "강원": { rank: 8, games: 24, points: 30, win: 8, draw: 6, lose: 10, goalsFor: 24, goalsAgainst: 30, goalDiff: -6 },
+  "제주": { rank: 9, games: 24, points: 29, win: 8, draw: 5, lose: 11, goalsFor: 27, goalsAgainst: 32, goalDiff: -5 },
+  "수원": { rank: 10, games: 24, points: 28, win: 7, draw: 7, lose: 10, goalsFor: 31, goalsAgainst: 32, goalDiff: -1 },
+  "안양": { rank: 11, games: 24, points: 27, win: 8, draw: 3, lose: 13, goalsFor: 29, goalsAgainst: 32, goalDiff: -3 },
+  "대구": { rank: 12, games: 24, points: 14, win: 3, draw: 5, lose: 16, goalsFor: 24, goalsAgainst: 45, goalDiff: -21 },
+};
+
+export default LeagueStandings;
