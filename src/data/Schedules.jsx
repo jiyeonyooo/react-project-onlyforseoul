145d130fbd826d940b8b360d0ee5c2de565b1b9f
jiyeonyooo <ji04230423@gmail.com>
2025-08-04 23:23:09 +0900

    chore: format code (spacing only)

--- src/data/Schedules.jsx
+++ src/data/Schedules.jsx
@@ -4,17 +4,48 @@ const schedules = {
     { text: "안데르손 선수 생일", type: "birth" }
   ],
   "2025-07-20": [
-    { text: "(H) vs 울산 | 22R", type: "match" }
+    { text: "(H) vs 울산 | 22R", type: "match", time: "19:00" }
   ],
   "2025-07-23": [
    { text: "(A) vs 제주 | 23R", type: "match" },
     { text: "최철원 선수 생일", type: "birth" }
   ],
+  "2025-07-27": [
+    { text: "(A) vs 대전 | 24R", type: "match" }
+  ],
   "2025-07-30": [
-    { text: "K리그 vs 뉴캐슬", type: "etc" }
+    { text: "K리그 vs 뉴캐슬 | 친선경기", type: "etc" }
   ],
   "2025-07-31": [
-    { text: "서울 vs FC 바르셀로나", type: "match" },
+    { text: "서울 vs 바르셀로나 | 친선경기", type: "etc" }
+  ],
+  "2025-08-08": [
+    { text: "(H) vs 대구 | 25R", type: "match" },
+  ],
+  "2025-08-17": [
+    { text: "(A) vs 김천 | 26R", type: "match" },
+  ],
+  "2025-08-24": [
+    { text: "(H) vs 울산 | 27R", type: "match" },
+  ],
+  "2025-08-31": [
+    { text: "(H) vs 안양 | 28R", type: "match" },
+  ],
+  "2025-09-13": [
+    { text: "(A) vs 강원 | 29R", type: "match" },
+  ],
+  "2025-09-21": [
+    { text: "(H) vs 광주 | 30R", type: "match" },
+  ],
+  "2025-09-27": [
+    { text: "(H) vs 전북 | 31R", type: "match" },
+  ],
+  "2025-10-05": [
+    { text: "(A) vs 수원 | 32R", type: "match" },
+  ],
+  "2025-10-18": [
+    { text: "(H) vs 포항 | 33R", type: "match" },
   ],
 };
+
 export default schedules;
